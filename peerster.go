51f84baf937e37955196c253fab4832f03a9c422
Alexis Montavon <alexis.montavon@epfl.ch>
2018-10-12 13:45:35 +0200

    fixed bug list

--- peerster.go
+++ peerster.go
@@ -13,13 +13,16 @@ func main() {
 	var name = flag.String("name", "Alexis-245433", "name of the gossiper")
 	var peers = flag.String("peers", "", "comma separated list of peers of the form ip:port")
 	var simple = flag.Bool("simple", false, "run gossiper in simple broadcast mode")
+	var webserver = flag.Bool("web", false, "run Peerster with the web server mode")
 	flag.Parse()
 
 	gos := gossiper.NewGossiper(*gossipAddr, *UIPort, *name, *peers, *simple)
 	clientReadBuffer := make([]byte, 4096)
 	peersReadBuffer := make([]byte, 4096)
 
-	go web.Run(gos)
+	if *webserver {
+		go web.Run(gos)
+	}
 	go gos.AntiEntropy()
 	go gos.ListenClient(clientReadBuffer)
 	gos.ListenPeers(peersReadBuffer)
